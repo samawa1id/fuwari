3919c5e785e0eb73f21366279aa01c7cfd152af7
Sama <165390112+samawa1id@users.noreply.github.com>
2026-01-15 15:07:47 +0200

    Update profile bio in config

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "0xSky",
-	bio: "Cybersecurity Student | DFIR | CTFs",
+	bio: "Programmer | DFIR CTF Player | ENTJ",
 	links: [
 		{
 			name: "Telegram",
