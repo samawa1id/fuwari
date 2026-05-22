85eaabeb9fab70522d4867076e1e42e00f2ff8dd
Sama <165390112+samawa1id@users.noreply.github.com>
2026-05-22 16:02:23 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/ppp.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/pppp.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "0xSky",
 	bio: "Programmer | CTF Player | ENTJ",
 	links: [
